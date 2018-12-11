f7ce3401154ae1f1d81de97f583b30fb2afb143e
bartdc <vespasiano@srrg.it>
2018-12-11 10:56:18 +0100

    fix dbow2 cmake

--- cmake_modules/FindDBoW2.cmake
+++ cmake_modules/FindDBoW2.cmake
@@ -10,8 +10,15 @@ FIND_PATH(DBoW2_INCLUDE_DIR
 
 if(DBoW2_INCLUDE_DIR)
   message("srrg_cmake_modules|found DBoW2:  ${DBoW2_INCLUDE_DIR}")
-  set(DBoW2_LIBRARY ${DBoW2_INCLUDE_DIR}/../build/libDBoW2.so)
-  message("srrg_cmake_modules|DBoW2 LIB:  ${DBoW2_LIBRARY}")
+  find_library(DBoW2_LIBRARY DBoW2
+    HINTS ${DBoW2_INCLUDE_DIR}/../lib)
+  if(DBoW2_LIBRARY)
+    message("srrg_cmake_modules|DBoW2 LIB:  ${DBoW2_LIBRARY}")
+  else()
+    set(DBoW2_LIBRARY ${DBoW2_INCLUDE_DIR}/../lib/libDBoW2.so)
+    message("srrg_cmake_modules|DBoW2 LIB not found | bdc HARDCODING")
+    message("srrg_cmake_modules|DBoW2 LIB:  ${DBoW2_LIBRARY}")
+  endif()
 else()
   message("srrg_cmake_modules|DBoW2 not found | bdc HARDCODING")
   set(DBoW2_INCLUDE_DIR ${DBoW2_DIR}/include)
