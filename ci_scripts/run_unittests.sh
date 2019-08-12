22524b084650cd39784ba821742f309197f1bb50
dom <schdomin@gmail.com>
2019-08-12 20:32:28 +0200

    now skipping unittests if tests folder is not existing

--- ci_scripts/run_unittests.sh
+++ ci_scripts/run_unittests.sh
@@ -7,27 +7,31 @@ if [ "$#" -ne 3 ]; then
 fi
 
 #ds set parameters
+echo "--------------------------------------------------------------------------------"
+echo "bash version: ${BASH_VERSION}"
 REPOSITORY_PATH="$1"
 WORKSPACE_PATH="$2"
 PROJECT_NAME="$3"
 
-#ds parse test binaries
+#ds compose test path and check if existing
 TEST_BINARIES_PATH="${REPOSITORY_PATH}/${PROJECT_NAME}/tests"
-TEST_BINARIES=($(ls ${TEST_BINARIES_PATH}))
+if [ -d "$TEST_BINARIES_PATH" ]; then
+  #ds parse test binaries from test path
+  TEST_BINARIES=($(ls ${TEST_BINARIES_PATH}))
+  echo "found test files in '${TEST_BINARIES_PATH}': "
+  echo "${TEST_BINARIES[@]} (unfiltered)"
+  
+  #ds call each binary (skipping all files that do not end in cpp nor start with test)
+  for TEST_BINARY in "${TEST_BINARIES[@]}"
+  do
+    #ds binary must start with test keyword and end in .cpp
+    TEST_BINARY_PREFIX=${TEST_BINARY:0:4}
+    TEST_BINARY_FILE_TYPE=${TEST_BINARY:${#TEST_BINARY}-4:4}
+    if [ ${TEST_BINARY_PREFIX} == "test" ] && [ ${TEST_BINARY_FILE_TYPE} == ".cpp" ]; then
+  	  ${WORKSPACE_PATH}/${PROJECT_NAME}/${TEST_BINARY:0:${#TEST_BINARY}-4}
+  	fi
+  done
+else 
+  echo "test directory: $TEST_BINARIES_PATH is not existing - skipping tests"
+fi
 echo "--------------------------------------------------------------------------------"
-echo "bash version: ${BASH_VERSION}"
-echo "found test files in '${TEST_BINARIES_PATH}': "
-echo "${TEST_BINARIES[@]} (unfiltered)"
-echo "--------------------------------------------------------------------------------"
-
-#ds call each binary (skipping all files that do not end in cpp nor start with test)
-for TEST_BINARY in "${TEST_BINARIES[@]}"
-do
-  #ds binary must start with test keyword and end in .cpp
-  TEST_BINARY_PREFIX=${TEST_BINARY:0:4}
-  TEST_BINARY_FILE_TYPE=${TEST_BINARY:${#TEST_BINARY}-4:4}
-  if [ ${TEST_BINARY_PREFIX} == "test" ] && [ ${TEST_BINARY_FILE_TYPE} == ".cpp" ]; then
-	  ${WORKSPACE_PATH}/${PROJECT_NAME}/${TEST_BINARY:0:${#TEST_BINARY}-4}
-	fi
-done
-
