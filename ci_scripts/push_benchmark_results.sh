99f1a900ef60434e2fdc2cd376186a49d0d6cf04
dom <schdomin@gmail.com>
2019-10-15 15:04:22 +0200

    softcoded benchmark result pushing, advanced sed magic

--- ci_scripts/push_benchmark_results.sh
+++ ci_scripts/push_benchmark_results.sh
@@ -2,8 +2,8 @@
 #ds this script is intended to be sourced!
 
 #ds check input parameters
-if [ "$#" -ne 2 ]; then
-  echo "ERROR: call as $0 PROJECT_NAME COMMIT_MESSAGE"
+if [ "$#" -ne 4 ]; then
+  echo "ERROR: call as $0 RESULT_REPOSITORY_PATH PROJECT_NAME DATASET_NAME COMMIT_MESSAGE"
   exit -1
 fi
 
@@ -16,21 +16,28 @@ fi
 #ds start
 echo -e "\e[1;96m--------------------------------------------------------------------------------\e[0m"
 echo -e "\e[1;96mbash version: ${BASH_VERSION}\e[0m"
-PROJECT_NAME="$1"
-COMMIT_MESSAGE="$2"
+RESULT_REPOSITORY_PATH="$1"
+PROJECT_NAME="$2"
+DATASET_NAME="$3"
+COMMIT_MESSAGE="$4"
 
-#ds update reference commit - currently locked in executor TODO change
-cd "/root/workspace/srrg2_executor"
-sed -i "s|${PROJECT_NAME}/commit/.*|${PROJECT_NAME}/commit/${CI_COMMIT_SHA}|g" "readme.md"
+#ds update reference commit in the provided result repository
+cd "$RESULT_REPOSITORY_PATH"
+sed -i "s|\[${DATASET_NAME}\]\: https://gitlab.com/srrg-software/${PROJECT_NAME}/commit/.*|\[${DATASET_NAME}\]\: https://gitlab.com/srrg-software/${PROJECT_NAME}/commit/${CI_COMMIT_SHA}|g" "readme.md"
 git config --global user.email "benchamin@srrg.com"
 git config --global user.name "benchamin"
 git add "results"
+ls -al
 
 #ds if the content has changed (does not have to be the case)
 if [[ $(git status) != *"nothing to commit, working tree clean"* ]]; then
   #ds push to remote without triggering it's ci
+  echo -e "\e[1;96mgit commit -am ${COMMIT_MESSAGE} [skip ci]\e[0m"
   git commit -am "${COMMIT_MESSAGE} [skip ci]"
+  echo -e "\e[1;96mgit pull --rebase\e[0m"
   git pull --rebase
+  echo -e "\e[1;96mgit push origin master\e[0m"
+  #ds push - can be ignored if there were no changes after pulling
   git push origin master
 else
   #ds ignore push
