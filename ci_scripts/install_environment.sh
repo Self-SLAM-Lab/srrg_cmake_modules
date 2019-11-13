f7ef37e9c8bce1e7362e4bc01487bbf02d238ab9
Sapienza Robust Robotics Group (SRRG) <srrg@dis.uniroma1.it>
2019-11-13 17:51:21 +0000

    stfu apt update and apt install

--- ci_scripts/install_environment.sh
+++ ci_scripts/install_environment.sh
@@ -16,10 +16,10 @@ echo -e "\e[1;96mbash version: ${BASH_VERSION}\e[0m"
 apt-mark hold ros-* > /dev/null
 
 #ds generic build dependencies (valid for ubuntu 16.04 and 18.04)
-apt update 
+apt update -qq
 #mc disabling this temporally
 # apt dist-upgrade -y
-apt install -y -q sudo ssh openssh-client git \
+apt install -y -qq sudo ssh openssh-client git \
     python-catkin-tools build-essential libeigen3-dev \
     libsuitesparse-dev libgtest-dev
 
