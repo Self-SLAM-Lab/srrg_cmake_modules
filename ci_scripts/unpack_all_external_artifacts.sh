79e23390a70c45632e659a62559ab98f9ec5a1a7
Sapienza Robust Robotics Group (SRRG) <srrg@dis.uniroma1.it>
2019-11-25 17:09:53 +0000

    Update unpack_all_external_artifacts.sh

--- ci_scripts/unpack_all_external_artifacts.sh
+++ ci_scripts/unpack_all_external_artifacts.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 #ds this script is intended to be sourced!
 
-cd "/root/workspace/src/$PROJECT_NAME"
+#ds check input parameters
+if [ "$#" -ne 4 ]; then
+  echo "ERROR: call as $0 PROJECT_NAME BRANCH_NAME JOB_NAME ACCESS_TOKEN"
+  exit -1
+fi
+
+#ds start
+echo -e "\e[1;96m--------------------------------------------------------------------------------\e[0m"
+echo -e "\e[1;96mbash version: ${BASH_VERSION}\e[0m"
+PROJECT_NAME="$1"
+BRANCH_NAME="$2"
+JOB_NAME="$3"
+TOKEN="$4"
+
+cd /root/workspace/src/$PROJECT_NAME
 
 SRRG_LIBS=`catkin list --this --rdeps | awk '/build_depend/,/run_depend/{print $2}' | xargs -0 echo | awk '/srrg2/{print $0}' |  tac`
 
 for LIB in $SRRG_LIBS;
-  do source ${SRRG_SCRIPT_PATH}/unpack_external_artifacts.sh "$LIB" "$CI_BUILD_REF_NAME" "$CI_JOB_NAME" "$ARTIFACT_PRIVATE_TOKEN";
+  do source ${SRRG_SCRIPT_PATH}/unpack_external_artifacts.sh "$LIB" "$BRANCH_NAME" "$JOB_NAME" "$TOKEN";
 done
