778551861cadad729d15ee8b8c26e7608d005a38
SRRG <srrg@diag.uniroma1.it>
2020-01-29 11:52:21 +0100

    push results on devel

--- ci_scripts/push_benchmark_results.sh
+++ ci_scripts/push_benchmark_results.sh
@@ -25,6 +25,7 @@ COMMIT_MESSAGE="$4"
 cd "$RESULT_REPOSITORY_PATH"
 git config --global user.email "benchamin@srrg.com"
 git config --global user.name "benchamin"
+git checkout devel
 git add "results"
 ls -al
 
