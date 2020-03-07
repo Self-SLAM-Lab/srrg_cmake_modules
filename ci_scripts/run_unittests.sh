ba2b76670724081966e733644a9db2a4f713fee4
SRRG <srrg@diag.uniroma1.it>
2020-03-07 11:12:06 +0100

    run_unittests script checks for tests present in the executable directory

--- ci_scripts/run_unittests.sh
+++ ci_scripts/run_unittests.sh
@@ -9,7 +9,7 @@ fi
 #ds set parameters
 echo -e "\e[1;96m--------------------------------------------------------------------------------\e[0m"
 echo -e "\e[1;96mbash version: ${BASH_VERSION}\e[0m"
-REPOSITORY_PATH="$1"
+REPOSITORY_PATH="$1"  #TODO remove this useless variable
 PROJECT_NAME="$2"
 WORKSPACE_PATH="$3"
 if [[ -z ${WORKSPACE_PATH} ]]; then
@@ -18,38 +18,31 @@ fi
 
 if [[ -z ${SRRG_SCRIPT_PATH} ]]; then
   echo -e "\e[1;91mSRRG_SCRIPT_PATH not set\e[0m"
+  return -1
   exit -1
 fi
 
-#ds compose test path and check if existing - with fallback for non-nested repositories
-TEST_BINARIES_PATH="${REPOSITORY_PATH}/${PROJECT_NAME}/tests"
-if [ ! -d "$TEST_BINARIES_PATH" ]; then
-  echo -e "\e[1;93mtest directory: $TEST_BINARIES_PATH is not existing - fallback to ${REPOSITORY_PATH}/tests\e[0m"
-  TEST_BINARIES_PATH="${REPOSITORY_PATH}/tests"
+EXECUTABLE_FOLDER="${WORKSPACE_PATH}/devel/${PROJECT_NAME}/lib/${PROJECT_NAME}/"
+if [ ! -d "$EXECUTABLE_FOLDER" ]; then
+  echo -e "\e[1;93mexecutable directory: $EXECUTABLE_FOLDER is not existing"
+  return -1
+  exit -1
 fi
 
-#ds check if binary path is valid now
-if [ -d "$TEST_BINARIES_PATH" ]; then
-  #ds parse test binaries from test path
-  TEST_BINARIES=($(ls ${TEST_BINARIES_PATH}))
-  echo -e "\e[1;96mfound test files in '${TEST_BINARIES_PATH}' (unfiltered): \e[0m"
-  echo "${TEST_BINARIES[@]}"
+TEST_BINARIES=($(ls ${EXECUTABLE_FOLDER}))
+echo -e "\e[1;96mfound test files in '${EXECUTABLE_FOLDER}' (unfiltered): \e[0m"
+echo "${TEST_BINARIES[@]}"
 
-  #ds call each binary (skipping all files that do not end in cpp nor start with test)
-  for TEST_BINARY in "${TEST_BINARIES[@]}"
-  do
-    #ds binary must start with test keyword and end in .cpp
-    TEST_BINARY_PREFIX=${TEST_BINARY:0:4}
-    TEST_BINARY_FILE_TYPE=${TEST_BINARY:${#TEST_BINARY}-4:4}
-    if [ ${TEST_BINARY_PREFIX} == "test" ] && [ ${TEST_BINARY_FILE_TYPE} == ".cpp" ]; then
-      source ${SRRG_SCRIPT_PATH}/run_executable.sh ${PROJECT_NAME} ${TEST_BINARY:0:${#TEST_BINARY}-4} ${WORKSPACE_PATH}
-      # echo ""
-      # TEXT_EXE="${WORKSPACE_PATH}/devel/${PROJECT_NAME}/lib/${PROJECT_NAME}/${TEST_BINARY:0:${#TEST_BINARY}-4}"
-      # echo -e "\e[1;96m${TEXT_EXE}\e[0m"
-      # ${TEXT_EXE}
-  	fi
-  done
-else
-  echo -e "\e[1;93mtest directory: $TEST_BINARIES_PATH is not existing - skipping tests (confirm if running this stage is necessary!)\e[0m"
-fi
+for TEST_BINARY in "${TEST_BINARIES[@]}"
+do
+  #ds binary must start with test keyword and end in .cpp
+  TEST_BINARY_PREFIX=${TEST_BINARY:0:4}
+  if [ ${TEST_BINARY_PREFIX} == "test" ]; then
+    source ${SRRG_SCRIPT_PATH}/run_executable.sh ${PROJECT_NAME} ${TEST_BINARY} ${WORKSPACE_PATH}
+    # echo ""
+    # TEXT_EXE="${WORKSPACE_PATH}/devel/${PROJECT_NAME}/lib/${PROJECT_NAME}/${TEST_BINARY:0:${#TEST_BINARY}-4}"
+    # echo -e "\e[1;96m${TEXT_EXE}\e[0m"
+    # ${TEXT_EXE}
+  fi
+done
 echo -e "\e[1;96m--------------------------------------------------------------------------------\e[0m"
