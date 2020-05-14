5ea0477a1a17344ab143137f02ca689b809f8523
SRRG <srrg@diag.uniroma1.it>
2020-05-14 20:04:10 +0200

    now laser single should work

--- ci_scripts/benchmark/find_dl.sh
+++ ci_scripts/benchmark/find_dl.sh
@@ -3,6 +3,7 @@
 cd ${WS}/src/
 git clone git@gitlab.com:srrg-software/srrg2_executor.git
 cd ${WS}
+catkin build srrg_cmake_modules --no-deps
 catkin build srrg2_executor --no-deps
 ${WS}/devel/srrg2_executor/lib/srrg2_executor/auto_dl_finder
 export DLC=${WS}/dl.conf
--- ci_scripts/benchmark/run_benchmark_app.sh
+++ ci_scripts/benchmark/run_benchmark_app.sh
@@ -2,20 +2,20 @@
 
 if [[ $# -ne 5 ]]; then
   echo "usage: $0 <dataset> <config> <gt_topic> <dl_source> <plot_mode {xy,xz,xyz}>"
-  exit -1;
   return;
+  exit -1;
 fi
 
-BLACK=$(tput setaf 0)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-BLUE=$(tput setaf 4)
-MAGENTA=$(tput setaf 5)
-CYAN=$(tput setaf 6)
-WHITE=$(tput setaf 7)
-RESET=$(tput sgr0)
-BOLD=$(tput bold)
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+RESET="\033[0m"
+BOLD="\033[1m"
 
 DATASET_NAME="$1"
 CONFIG_NAME="$2"
@@ -28,8 +28,8 @@ TRAJ_FORMAT="tum"
 #find config in workspace
 CONFIG="$(find $CI_PROJECT_DIR -type f -name ${CONFIG_NAME})"
 if [[ ! -f ${CONFIG}  ]]; then
-  echo "Config "$CYAN $CONFIG_NAME" $RED$BOLD NOT FOUND $RESET in "$YELLOW $(pwd)
-  echo $RESET
+  echo -e "Config "$CYAN $CONFIG_NAME" $RED$BOLD NOT FOUND $RESET in "$YELLOW $CI_PROJECT_DIR
+  echo -e $RESET
   return;
   exit -1;
 fi
@@ -37,30 +37,30 @@ CONFIG=$(realpath "${CONFIG}")
 
 #find dataset in standard dataset directory
 if [[ -z "${DATASET_PATH}" ]]; then
-  echo "$YELLOW DATASET_PATH $RESET variable $RED$BOLD NOT SET $RESET "
-  echo "Please set it before calling this script"
-  echo $RESET
+  echo -e "$YELLOW DATASET_PATH $RESET variable $RED$BOLD NOT SET $RESET "
+  echo -e "Please set it before calling this script"
+  echo -e $RESET
   return;
   exit -1;
 fi
 DATASET="$(find $DATASET_PATH -type f -name ${DATASET_NAME})"
 if [[ ! -f ${DATASET}  ]]; then
-  echo "Dataset "$CYAN $DATASET_NAME" $RED$BOLD NOT FOUND $RESET in "$YELLOW $(pwd)
-  echo $RESET
+  echo -e "Dataset "$CYAN $DATASET_NAME" $RED$BOLD NOT FOUND $RESET in "$YELLOW $DATASET_PATH
+  echo -e $RESET
   return;
   exit -1;
 fi
 DATASET=$(realpath "${DATASET}")
 
-echo "dataset: $CYAN${DATASET}$RESET"
-echo "config: $CYAN${CONFIG}$RESET"
-echo "gt_topic: $CYAN${GT_TOPIC}$RESET"
+echo -e "dataset: $CYAN${DATASET}$RESET"
+echo -e "config: $CYAN${CONFIG}$RESET"
+echo -e "gt_topic: $CYAN${GT_TOPIC}$RESET"
 
 roscore&
 ROSCORE_PID=$!
 sleep 2
 
-DEVEL_PATH="${WS}/srrg2/devel/"
+DEVEL_PATH="${WS}/devel/"
 if [ ${DATASET: -4} == ".bag" ]; then
   EXEC_PATH="${DEVEL_PATH}srrg2_core_ros/lib/srrg2_core_ros/"
   ${EXEC_PATH}extract_gt_from_ros -f ${TRAJ_FORMAT} -i "${DATASET}" -t "${GT_TOPIC}" -o "${GT_FILE}"
@@ -73,30 +73,29 @@ ${EXEC_PATH}app_benchmark -dlc ${DL_SOURCE} -c ${CONFIG} -d "${DATASET}" -f ${TR
 
 kill -9 $ROSCORE_PID
 
+cat out_${TRAJ_FORMAT}.txt
+
 if [[ ! $(pip3 list --format=columns | grep evo)  ]]; then
-  echo "evo not installed"
-  echo "run: pip3 install evo --upgrade --no-binary evo"
+  echo -e "evo not installed"
+  echo -e "run: pip3 install evo --upgrade --no-binary evo"
   exit -1
   return;
 fi
 
 RESULTS_APE="results_ape"
 PLOT_APE="plot_ape"
-evo_ape ${TRAJ_FORMAT} ${GT_FILE} out_${TRAJ_FORMAT}.txt -va --plot_mode ${PLOT_MODE} --save_results ${RESULTS_APE}.zip --no_warnings --save_plot ${PLOT_APE}.pdf
+evo_ape ${TRAJ_FORMAT} ${GT_FILE} out_${TRAJ_FORMAT}.txt -va --plot_mode ${PLOT_MODE} --save_results ${RESULTS_APE}.zip --no_warnings --save_plot ${PLOT_APE}.png
 
 unzip -p ${RESULTS_APE}.zip stats.json > ${RESULTS_APE}.json
-pdftoppm ${PLOT_APE}.pdf ${PLOT_APE} -png -f 2 -singlefile
 
-rm ${RESULTS_APE}.zip ${PLOT_APE}.pdf
+rm ${RESULTS_APE}.zip
 
 RESULTS_RPE="results_rpe"
 PLOT_RPE="plot_rpe"
-evo_rpe ${TRAJ_FORMAT} ${GT_FILE} out_${TRAJ_FORMAT}.txt -va --plot_mode ${PLOT_MODE} --save_results ${RESULTS_RPE}.zip --no_warnings --save_plot ${PLOT_RPE}.pdf
+evo_rpe ${TRAJ_FORMAT} ${GT_FILE} out_${TRAJ_FORMAT}.txt -va --plot_mode ${PLOT_MODE} --save_results ${RESULTS_RPE}.zip --no_warnings --save_plot ${PLOT_RPE}.png
 
 unzip -p ${RESULTS_RPE}.zip stats.json > ${RESULTS_RPE}.json
-pdftoppm ${PLOT_RPE}.pdf ${PLOT_RPE} -png -f 2 -singlefile
 
-rm ${RESULTS_RPE}.zip ${PLOT_RPE}.pdf
-
+rm ${RESULTS_RPE}.zip 
 export RESULTS_APE=$(realpath ${RESULTS_APE}.json)
 export RESULTS_RPE=$(realpath ${RESULTS_RPE}.json)
