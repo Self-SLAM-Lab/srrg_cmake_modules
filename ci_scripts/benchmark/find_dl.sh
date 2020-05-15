82640b8cf168186dee52a16ff61ea5b10cc47d9e
SRRG <srrg@diag.uniroma1.it>
2020-05-15 19:09:23 +0200

    moving to correct branch

--- ci_scripts/benchmark/find_dl.sh
+++ ci_scripts/benchmark/find_dl.sh
@@ -2,6 +2,8 @@
 
 cd ${WS}/src/
 git clone git@gitlab.com:srrg-software/srrg2_executor.git
+cd srrg2_executor
+git checkout $CI_BUILD_REF_NAME
 cd ${WS}
 catkin build srrg_cmake_modules --no-deps
 catkin build srrg2_executor --no-deps
